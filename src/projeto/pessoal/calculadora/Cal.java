7fb5afe5f98b88c06fac5e7961c351e99c4e85d0
Mateus Lopes <mateustlopes11@gmail.com>
2024-06-01 04:16:08 -0300

    projeto calculadora

---
+++ src/projeto/pessoal/calculadora/Cal.java
@@ -0,0 +1,90 @@
+package projeto.pessoal.calculadora;
+
+import java.util.Scanner;
+//
+public class Cal{
+    public static double soma(double num1,double num2) {
+        return num1 + num2;
+    }
+    public static double subtracao(double num1, double num2){
+        return num1 - num2;
+    }
+    public static double multiplicacao(double num1, double num2){
+        return num1 * num2;
+    }
+    public static double divisao (double num1, double num2){
+        return num1/num2;
+    }
+
+    public static void main(String[] args) {
+
+    
+        boolean calculadoraNum = true;
+        Scanner scan = new Scanner(System.in);
+
+       
+
+
+        while (calculadoraNum) {
+
+        System.out.println("\n opções: ");
+        System.out.println("1. SOMAR ");
+        System.out.println("2. SUBTRAIR");
+        System.out.println("3. MULTIPLICAR");
+        System.out.println("4. DIVIDIR");
+        System.out.println("5. SAIR");
+        int operacao = scan.nextInt();
+
+        if (operacao == 5) {
+            calculadoraNum = false;
+            System.out.println("saindo...");
+            break;
+
+            
+        } 
+
+
+
+        if (operacao == 1) {
+           System.out.println("digite um numero: ");
+           double num1 = scan.nextDouble();
+           System.out.println("Digite o segundo numero: ");
+           double num2 = scan.nextDouble();
+           System.out.println("O resultado de "+num1+" somado por "+ num2 + " é: "+soma(num1, num2));
+           
+        } else if (operacao == 2) {
+            System.out.println("digite um numero: ");
+            double num1 = scan.nextDouble();
+            System.out.println("digite o segundo numero: ");
+            double num2 = scan.nextDouble();
+            System.out.println("O resultado de "+num1+" subtraido por "+ num2 + " é: "+subtracao(num1, num2));
+
+            
+        } else if (operacao == 3) {
+            System.out.println("Digite o primeiro numero: ");
+            double num1 = scan.nextDouble();
+            System.out.println("Digite o segundo numero: ");
+            double num2 = scan.nextDouble();
+            System.out.println("O resultado de "+num1+" multiplicado por "+ num2 + " é: "+multiplicacao(num1, num2));
+            
+        } else if (operacao == 4) {
+            System.out.println("digite o primerio numero: ");
+            double num1 = scan.nextDouble();
+            System.out.println("digite o segundo numero: ");
+            double num2 = scan.nextDouble();
+            System.out.println("O resultado de "+num1+" dividido por "+ num2 + " é: "+ divisao(num1, num2));
+            
+        } else if (operacao != 1 && operacao != 2 && operacao != 3 && operacao != 4 && operacao != 5){
+            System.out.println("DADOS NÃO ENCONTRADOS, TENTE NOVAMENTE!!");
+            }
+
+       
+    }
+        
+
+
+
+        scan.close();
+    }
+
+}
